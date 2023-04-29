bf65da29271ee2f811472f02aa9552191dd47c5b
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-04-28 21:47:44 -0400

    macros: import field macro from old codebase

--- src/clj/app/macros.clj
+++ src/clj/app/macros.clj
@@ -11,3 +11,42 @@
   (let [xs (interleave (mapv keyword vars)
                        (vec vars))]
     `(assoc ~'args ~@xs)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def field-names
+  ["underlying_symbol"
+   "quote_datetime"
+   "root"
+   "expiration"
+   "strike"
+   "option_type"
+   "open"
+   "high"
+   "low"
+   "close"
+   "trade_volume"
+   "bid_size"
+   "bid"
+   "ask_size"
+   "ask"
+   "underlying_bid"
+   "underlying_ask"
+   "implied_underlying_price"
+   "active_underlying_price"
+   "implied_volatility"
+   "delta"
+   "gamma"
+   "theta"
+   "vega"
+   "rho"
+   "open_interest"])
+(def field-name-indexes (into {} (map-indexed (fn [i x] [x i]) field-names)))
+
+(defmacro field [arr k]
+  (let [i (get field-name-indexes k)
+        transformer (case k
+                      "strike" `(Float/parseFloat)
+                      `(identity))]
+    `(-> (aget ~arr ~i)
+         ~@transformer)))
